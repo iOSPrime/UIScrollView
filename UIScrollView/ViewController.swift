fd8f31fedf8dfaae3497f0b492146fac597aef88
Ashish Patel <a.patel@capitalone.com>
2016-12-07 09:26:56 -0800

    dd tap gesture + add keyboard notification

--- UIScrollView/ViewController.swift
+++ UIScrollView/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ///step.1 Add tap gesture to view
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapView(gesture:)))
+        view.addGestureRecognizer(tapGesture)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        ///step.8 Add observers
+        addObservers()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        ///step.7 Remove observers
+        removeObservers()
+    }
+
+    ///step.2 Add method to handle tap event on the view and dismiss keyboard.
+    func didTapView(gesture: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+
+    ///step.3 Add observers for 'UIKeyboardWillShow' and 'UIKeyboardWillHide' notification.
+    func addObservers() {
+        NotificationCenter.default.addObserver(forName: .UIKeyboardWillShow, object: nil, queue: nil) { notification in
+            self.keyboardWillShow(notification: notification)
+        }
+
+        NotificationCenter.default.addObserver(forName: .UIKeyboardWillHide, object: nil, queue: nil) {
+            notification in
+            self.keyboardWillHide(notification: notification)
+        }
+    }
+    ///step.6 Method to remove observers
+    func removeObservers() {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    ///step.4 Add method to handle keyboardWillShow notification, we're using this method to adjust scrollview to show hidden textfield under keyboard.
+    func keyboardWillShow(notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let frame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        let contentInset = UIEdgeInsets(top: 0, left: 0, bottom: frame.height, right: 0)
+        scrollView.contentInset = contentInset
+    }
+
+    ///step.5 Method to reset scrollview when keyboard is hidden.
+    func keyboardWillHide(notification: Notification) {
+        scrollView.contentInset = UIEdgeInsets.zero
+    }
 
 }
-
